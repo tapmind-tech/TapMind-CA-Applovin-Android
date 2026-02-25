7c2fbcd7bc6b3ebad1f7dad9745510bdb0822694
WRP <piyush.kapadiya@gmail.com>
2026-02-25 17:49:46 +0530

    1.0.8

--- customadapter-applovin/build.gradle.kts
+++ customadapter-applovin/build.gradle.kts
@@ -1,13 +1,15 @@
 import com.android.build.gradle.ProguardFiles.getDefaultProguardFile
 
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.gradleup.nmcp")
     id("maven-publish")
+    id("signing")
 }
 
 android {
-    namespace = "com.tapmimd.ads.mediation.adapter"
+    namespace = "com.ironsource.adapters.custom.tapmind_applovin"
     compileSdk = 36
     defaultConfig {
         minSdk = 23
@@ -38,28 +40,84 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
 
-    compileOnly("com.applovin:applovin-sdk:13.5.0")
-    implementation(project(":TapMindSdk"))
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+
+    api("com.applovin:applovin-sdk:13.6.0")
+//    api(project(":TapMindSdk"))
+    api("io.github.ravirising26:tapmindsdk:1.0.12")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components["release"])
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            create<MavenPublication>("release") {
+//                from(components["release"])
+//
+//                groupId = "com.github.TapMind"
+//                artifactId = "CustomAdapter-appLovin"
+//                version = "1.0.0"
+//            }
+//        }
+//    }
+//}
 
-                groupId = "com.github.TapMind"
-                artifactId = "CustomAdapter-appLovin"
-                version = "1.0.0"
-            }
-        }
-    }
-}
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            create<MavenPublication>("release") {
+//                from(components["release"])
+//
+//                groupId = "io.github.ravirising26"
+//                artifactId = "customadapter-applovin"
+//                version = "1.0.1"
+//
+//                pom {
+//                    name.set("customadapterAppLovin")
+//                    description.set("TapMind Ads mediation SDK")
+//                    url.set("https://github.com/TapMind/TapmindSdk")
+//
+//                    licenses {
+//                        license {
+//                            name.set("Apache-2.0")
+//                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+//                        }
+//                    }
+//
+//                    developers {
+//                        developer {
+//                            id.set("tapmind")
+//                            name.set("Ravi")
+//                            email.set("ravi.rising26@gmail.com")
+//                        }
+//                    }
+//
+//                    scm {
+//                        connection.set("scm:git:github.com/TapMind/TapmindSdk.git")
+//                        developerConnection.set("scm:git:ssh://github.com/TapMind/TapmindSdk.git")
+//                        url.set("https://github.com/TapMind/TapmindSdk")
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    nmcp {
+//        publish("release") {
+//            username = project.findProperty("ossrhUsername") as String?
+//            password = project.findProperty("ossrhPassword") as String?
+//            publicationType = "AUTOMATIC"
+//        }
+//    }
+//}
+//
+//signing {
+//    useGpgCmd()
+//    sign(publishing.publications)
+//}
--- customadapter-applovin/src/main/java/com/tapmimd/ads/mediation/adapter/TapMindAdapterApplovin.kt
+++ customadapter-applovin/src/main/java/com/tapmimd/ads/mediation/adapter/TapMindAdapterApplovin.kt
@@ -1,49 +1,53 @@
 package com.tapmimd.ads.mediation.adapter
 
 import android.app.Activity
+import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.applovin.mediation.MaxAdFormat
 import com.applovin.mediation.adapter.MaxAdViewAdapter
 import com.applovin.mediation.adapter.MaxAdapter
 import com.applovin.mediation.adapter.MaxAdapter.InitializationStatus
-import com.applovin.mediation.adapter.MaxAppOpenAdapter
+import com.applovin.mediation.adapter.MaxAdapterError
 import com.applovin.mediation.adapter.MaxInterstitialAdapter
-import com.applovin.mediation.adapter.MaxNativeAdAdapter
 import com.applovin.mediation.adapter.MaxRewardedAdapter
+import com.applovin.mediation.adapter.MaxSignalProvider
 import com.applovin.mediation.adapter.listeners.MaxAdViewAdapterListener
 import com.applovin.mediation.adapter.listeners.MaxAppOpenAdapterListener
 import com.applovin.mediation.adapter.listeners.MaxInterstitialAdapterListener
 import com.applovin.mediation.adapter.listeners.MaxNativeAdAdapterListener
 import com.applovin.mediation.adapter.listeners.MaxRewardedAdapterListener
+import com.applovin.mediation.adapter.listeners.MaxSignalCollectionListener
 import com.applovin.mediation.adapter.parameters.MaxAdapterInitializationParameters
 import com.applovin.mediation.adapter.parameters.MaxAdapterResponseParameters
+import com.applovin.mediation.adapter.parameters.MaxAdapterSignalCollectionParameters
 import com.applovin.mediation.adapters.MediationAdapterBase
 import com.applovin.sdk.AppLovinSdk
 
 class TapMindAdapterApplovin(appLovinSdk: AppLovinSdk) : MediationAdapterBase(appLovinSdk),
-    MaxAdViewAdapter, MaxNativeAdAdapter, MaxAppOpenAdapter, MaxInterstitialAdapter,MaxRewardedAdapter {
+    MaxInterstitialAdapter, MaxRewardedAdapter, MaxAdViewAdapter, MaxSignalProvider {
 
     private val TAG = "APP@@@"
     private val TAG1 = "TapMindAdapterApplovin"
 
     override fun initialize(
-        maxAdapterInitializationParameters: MaxAdapterInitializationParameters,
+        parameters: MaxAdapterInitializationParameters,
         activity: Activity,
         callback: MaxAdapter.OnCompletionListener?
     ) {
-        callback?.onCompletion(InitializationStatus.INITIALIZING,"success")
+        Log.d(TAG, "initialize: AppLovin 321")
+        callback?.onCompletion(InitializationStatus.INITIALIZED_SUCCESS, null)
     }
 
-    override fun getSdkVersion(): String? {
+    override fun getSdkVersion(): String {
         return "1.0.0"
     }
 
-    override fun getAdapterVersion(): String? {
-        return "1.0.0.0"
+    override fun getAdapterVersion(): String {
+        return "1.0.0"
     }
 
     override fun onDestroy() {
-       
     }
 
     override fun loadAdViewAd(
@@ -53,7 +57,75 @@ class TapMindAdapterApplovin(appLovinSdk: AppLovinSdk) : MediationAdapterBase(ap
         maxAdViewAdapterListener: MaxAdViewAdapterListener
     ) {
         Log.d(TAG, "$TAG1 loadAdViewAd")
-        val data = AppLovinBannerCustomEventLoader.getInstance(maxAdapterResponseParameters,maxAdFormat,activity,maxAdViewAdapterListener)
+        val data = AppLovinBannerCustomEventLoader.getInstance(
+            maxAdapterResponseParameters,
+            maxAdFormat,
+            activity,
+            object : MaxAdViewAdapterListener {
+                override fun onAdViewAdLoaded(p0: View?) {
+                    maxAdViewAdapterListener.onAdViewAdLoaded(p0)
+                }
+
+                override fun onAdViewAdLoaded(p0: View?, p1: Bundle?) {
+                    maxAdViewAdapterListener.onAdViewAdLoaded(p0, p1)
+                }
+
+                override fun onAdViewAdLoadFailed(p0: MaxAdapterError?) {
+                    maxAdViewAdapterListener.onAdViewAdLoadFailed(p0)
+                }
+
+                override fun onAdViewAdDisplayed() {
+                    maxAdViewAdapterListener.onAdViewAdDisplayed()
+                }
+
+                override fun onAdViewAdDisplayed(p0: Bundle?) {
+                    maxAdViewAdapterListener.onAdViewAdDisplayed(p0)
+                }
+
+                override fun onAdViewAdDisplayFailed(p0: MaxAdapterError?) {
+                    maxAdViewAdapterListener.onAdViewAdDisplayFailed(p0)
+                }
+
+                override fun onAdViewAdDisplayFailed(
+                    p0: MaxAdapterError?,
+                    p1: Bundle?
+                ) {
+                    maxAdViewAdapterListener.onAdViewAdDisplayFailed(p0, p1)
+                }
+
+                override fun onAdViewAdClicked() {
+                    maxAdViewAdapterListener.onAdViewAdClicked()
+                }
+
+                override fun onAdViewAdClicked(p0: Bundle?) {
+                    maxAdViewAdapterListener.onAdViewAdClicked(p0)
+                }
+
+                override fun onAdViewAdHidden() {
+                    maxAdViewAdapterListener.onAdViewAdHidden()
+                }
+
+                override fun onAdViewAdHidden(p0: Bundle?) {
+                    maxAdViewAdapterListener.onAdViewAdHidden(p0)
+                }
+
+                override fun onAdViewAdExpanded() {
+                    maxAdViewAdapterListener.onAdViewAdExpanded()
+                }
+
+                override fun onAdViewAdExpanded(p0: Bundle?) {
+                    maxAdViewAdapterListener.onAdViewAdExpanded(p0)
+                }
+
+                override fun onAdViewAdCollapsed() {
+                    maxAdViewAdapterListener.onAdViewAdCollapsed()
+                }
+
+                override fun onAdViewAdCollapsed(p0: Bundle?) {
+                    maxAdViewAdapterListener.onAdViewAdCollapsed(p0)
+                }
+            }
+        )
         data.loadAdd()
     }
 
@@ -63,7 +135,11 @@ class TapMindAdapterApplovin(appLovinSdk: AppLovinSdk) : MediationAdapterBase(ap
         maxNativeAdapterListener: MaxNativeAdAdapterListener?
     ) {
         Log.d(TAG, "$TAG1 loadNativeAdMapper")
-        AppLovinNativeCustomEventLoader.getInstance(maxAdapterResponseParameters,activity,maxNativeAdapterListener).loadAd()
+        AppLovinNativeCustomEventLoader.getInstance(
+            maxAdapterResponseParameters,
+            activity,
+            maxNativeAdapterListener
+        ).loadAd()
     }
 
     override fun loadInterstitialAd(
@@ -72,7 +148,11 @@ class TapMindAdapterApplovin(appLovinSdk: AppLovinSdk) : MediationAdapterBase(ap
         maxInterstitialAdapterListener: MaxInterstitialAdapterListener
     ) {
         Log.d(TAG, "$TAG1 loadInterstitialAd")
-        AppLovinInterstitialCustomEventLoader.getInstance(maxAdapterResponseParameters,activity,maxInterstitialAdapterListener).loadAd()
+        AppLovinInterstitialCustomEventLoader.getInstance(
+            maxAdapterResponseParameters,
+            activity,
+            maxInterstitialAdapterListener
+        ).loadAd()
     }
 
     override fun showInterstitialAd(
@@ -81,9 +161,12 @@ class TapMindAdapterApplovin(appLovinSdk: AppLovinSdk) : MediationAdapterBase(ap
         maxInterstitialAdapterListener: MaxInterstitialAdapterListener?
     ) {
         Log.d(TAG, "$TAG1 showInterstitialAd")
-        AppLovinInterstitialCustomEventLoader.getInstance(maxAdapterResponseParameters,activity,maxInterstitialAdapterListener!!).showAd(activity!!)
+        AppLovinInterstitialCustomEventLoader.getInstance(
+            maxAdapterResponseParameters,
+            activity,
+            maxInterstitialAdapterListener!!
+        ).showAd(activity!!)
     }
-
 
     override fun loadAppOpenAd(
         maxAdapterResponseParameters: MaxAdapterResponseParameters,
@@ -101,18 +184,19 @@ class TapMindAdapterApplovin(appLovinSdk: AppLovinSdk) : MediationAdapterBase(ap
 
     }
 
-
-
     override fun loadRewardedAd(
         maxAdapterResponseParameters: MaxAdapterResponseParameters?,
         activity: Activity?,
         maxRewardedAdapterListener: MaxRewardedAdapterListener?
     ) {
         Log.d(TAG, "$TAG1 loadRewardedAd")
-        val data = AppLovinRewardedCustomEventLoader.getInstance(maxAdapterResponseParameters,activity,maxRewardedAdapterListener!!)
+        val data = AppLovinRewardedCustomEventLoader.getInstance(
+            maxAdapterResponseParameters,
+            activity,
+            maxRewardedAdapterListener!!
+        )
         data.loadAd()
     }
-
 
     override fun showRewardedAd(
         maxAdapterResponseParameters: MaxAdapterResponseParameters?,
@@ -120,8 +204,19 @@ class TapMindAdapterApplovin(appLovinSdk: AppLovinSdk) : MediationAdapterBase(ap
         maxRewardedAdapterListener: MaxRewardedAdapterListener?
     ) {
         Log.d(TAG, "$TAG1 showRewardedAd")
-        val data = AppLovinRewardedCustomEventLoader.getInstance(maxAdapterResponseParameters,activity,maxRewardedAdapterListener!!)
+        val data = AppLovinRewardedCustomEventLoader.getInstance(
+            maxAdapterResponseParameters,
+            activity,
+            maxRewardedAdapterListener!!
+        )
         data.showAd(activity!!)
     }
 
+    override fun collectSignal(
+        parameters: MaxAdapterSignalCollectionParameters?,
+        activity: Activity?,
+        callback: MaxSignalCollectionListener?
+    ) {
+        Log.d(TAG, "collectSignal: AppLovin 321")
+    }
 }
